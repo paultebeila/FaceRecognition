e0dc4f56c1bfd83841f14a0d670d5327b165d900
Paul <paultebeila@gmail.com>
2023-06-09 15:55:59 +0200

    Added encodings

--- EncodeGenerators.py
+++ EncodeGenerators.py
@@ -1,15 +1,35 @@
 import cv2
 import face_recognition
 import pickle
+import os
 
 
 #importing the mode images into a list
-folderModePath = 'Resources/Modes'
-modePathList = os.listdir(folderModePath)
-imgModeList = []
+folderPath = 'Images'
+pathList = os.listdir(folderPath)
+imgList = []
+studentId =[]
 
-for path in modePathList:
-    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(pathList)
+
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    # name = os.path.splitext(path)[0]
+    studentId.append(os.path.splitext(path)[0])
     
     
-# print(len(imgModeList))
+    
+print(studentId)
+
+def findEncoding(imageList):
+    encodeList = []
+    for img in imageList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #converting the color from BGR to RGB
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+print("Encoding started.....")
+encodeListKnown = findEncoding(imgList)
+print(encodeListKnown)
+print("Encoding complete")
