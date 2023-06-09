21ff26555c372f5131a7ec33b5e3febffa942e90
Paul <paultebeila@gmail.com>
2023-06-09 15:01:21 +0200

    Face recognition module added

---
+++ EncodeGenerators.py
@@ -0,0 +1,15 @@
+import cv2
+import face_recognition
+import pickle
+
+
+#importing the mode images into a list
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+    
+    
+# print(len(imgModeList))
