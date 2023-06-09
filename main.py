37362aa5c9454f2a1118899acfcf22574473ec1a
Paul <paultebeila@gmail.com>
2023-06-09 16:42:08 +0200

    Face is recognized

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import os
 import pickle
+import face_recognition
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -33,8 +35,26 @@ print("Encoded file loaded")
 while True:
     success, img = cap.read()
     
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodingCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+    
     imgBackground[162:162+480, 55:55+640] = img
     imgBackground[44:44+633, 808:808+414] = imgModeList[3]
+    
+    for encodeFace, faceLoc in zip(encodingCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("matches: ", matches)
+        # print("Face Distance: ", faceDis)
+        
+        matchIndex = np.argmin(faceDis)
+        
+        if matches[matchIndex]:
+            print(studentId[matchIndex], " was detected")
+    
     # cv2.imshow("Face Attendance", img)
     cv2.imshow("Background", imgBackground)
     cv2.waitKey(1)
